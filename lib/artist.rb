e73554280a57ef919b4ba91462570e0a45eaf6a1
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-06 23:33:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,9 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    artist.all = self.all.find {|artist| artist.name == artist_name}
-    if 
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
     else
       new_artist = self.new(artist_name)
       new_artist.save
